1f2f4e455de8eaff31df50f9077a06a41c1363ab
okcoder <okcoder@live.com>
2023-02-27 12:28:40 +0900

    Spring initializr

---
+++ src/main/java/org/okcoder/demo/springmysqlaction/SpringMysqlActionApplication.java
@@ -0,0 +1,13 @@
+package org.okcoder.demo.springmysqlaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMysqlActionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMysqlActionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/okcoder/demo/springmysqlaction/SpringMysqlActionApplicationTests.java
@@ -0,0 +1,13 @@
+package org.okcoder.demo.springmysqlaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMysqlActionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
